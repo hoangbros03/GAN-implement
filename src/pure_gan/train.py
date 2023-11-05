1d0d0d41b4a5abea6f4d598655b628ee0b2c76aa
hoangbros03 <87892619+hoangbros03@users.noreply.github.com>
2023-11-05 23:24:49 +0700

    model auto save

--- src/pure_gan/train.py
+++ src/pure_gan/train.py
@@ -1,6 +1,7 @@
 import argparse
 from pathlib import Path
 import sys
+import datetime
 
 import numpy as np
 import torch
@@ -101,7 +102,7 @@ class Discriminator(nn.Module):
         img = self.l_final(img)
         return img
 
-def train(dataloader, epochs, latent_dim, img_shape, batch_size, learning_rate):
+def train(dataloader, epochs, latent_dim, img_shape, batch_size, learning_rate, output_model_dir):
     """_summary_ TODO
 
     Args:
@@ -148,10 +149,15 @@ def train(dataloader, epochs, latent_dim, img_shape, batch_size, learning_rate):
         g_loss.backward()
         g_optimizer.step()
 
-        print(f"Epoch: {epoch}/{epochs}, g_loss: {g_loss.item()}")
+        print(f"Epoch: {epoch}/{epochs}, g_loss: {g_loss.item()}, d_loss: {d_loss.item()}")
         wandb.log({
-          "Epoch": epoch, "Total epoch": epochs, "g_loss": g_loss.item()
+          "Epoch": epoch, "Total epoch": epochs, "g_loss": g_loss.item(), "d_loss": d_loss.item()
             })
+    
+        # Output the model
+        if (epoch+1) % 50 == 0:
+            torch.save(generator_model.state_dict(), f"{output_model_dir}/{str(datetime_now)}_{str(epoch+1)}.pth")
+
         
 if __name__ == "__main__":
     # Parser
@@ -162,13 +168,17 @@ if __name__ == "__main__":
     parser.add_argument("--cpus", type=int, default=8, help="number of cpu threads to use during batch generation")
     parser.add_argument("--latent_dim", type=int, default=100, help="dimensionality of the latent space")
     parser.add_argument("--sample_interval", type=int, default=400, help="interval betwen image samples")
+    parser.add_argument("--output_model_dir", type=str, default="models", help="Output model dir")
     args = parser.parse_args()
 
     # CONSTANT VARIABLE
     IMG_SHAPE = (1,28,28)
     IMG_SHAPE = (1,28,28)
 
+
     # Create dir
+    datetime_now = datetime.datetime.now().isoformat()
+    check_and_create_dir(args.output_model_dir)
     check_and_create_dir("gen_images")
 
     # Dataloader
@@ -188,5 +198,5 @@ if __name__ == "__main__":
     })
 
     # Train
-    train(dataloader, args.epochs, args.latent_dim, IMG_SHAPE, args.batch_size, args.lr)
+    train(dataloader, args.epochs, args.latent_dim, IMG_SHAPE, args.batch_size, args.lr, args.output_model_dir)
     
