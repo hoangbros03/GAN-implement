55037aeb1e4b66f7f1fac35c31ee107001609a37
Nguyen Binh Nguyen <nguyenrtm@gmail.com>
2023-11-15 23:33:57 +0700

    edit eval

--- evaluate/evaluate.py
+++ evaluate/evaluate.py
@@ -1,5 +1,14 @@
+import torch
 from torchmetrics.image.kid import KernelInceptionDistance
 from torchmetrics.image.fid import FrechetInceptionDistance
+
+def convert_dataloader_to_sample(dataloader):
+    for i, batch_data in enumerate(dataloader):
+        if i < 16:
+            if i == 0:
+                gt = batch_data[0]
+            else: 
+                gt = torch.vstack((gt, batch_data[0]))
 
 def kernel_inception_distance(prediction, gt):
     kid = KernelInceptionDistance(subset_size=1)
